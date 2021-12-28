73edcaba4b840e7132b37537cff76aa5951f6267
hieupham-thedev <hieu-pham.dev@outlook.com.vn>
2021-12-28 20:53:06 +0700

    Handle check login and check role to redirect page

--- src/main/java/com/demoipm/config/WebSecurityConfig.java
+++ src/main/java/com/demoipm/config/WebSecurityConfig.java
@@ -1,21 +1,32 @@
 package com.demoipm.config;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import java.io.IOException;
+import java.util.Set;
+
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -44,16 +55,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
 
-		http.authorizeRequests().antMatchers("/candidate-info").access("hasAnyRole('ROLE_ADMIN', 'ROLE_HR', 'ROLE_INTERVIEWER')");
-
-		http.authorizeRequests().antMatchers("/user-info").access("hasRole('ROLE_ADMIN')");
-
 		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
 
 		http.authorizeRequests().and().formLogin()
 				.loginProcessingUrl("/j_spring_security_check")
 				.loginPage("/login")
-				.defaultSuccessUrl("/candidate-info")
+				.successHandler(customSuccessHandler())
 				.failureUrl("/login?error=true")
 				.usernameParameter("username")
 				.passwordParameter("password")
@@ -70,6 +77,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
 		db.setDataSource(dataSource);
 		return db;
+	}
+
+	@Bean
+	public SavedRequestAwareAuthenticationSuccessHandler customSuccessHandler() {
+		return new SavedRequestAwareAuthenticationSuccessHandler() {
+			@Override
+			public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+				Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+				if (roles.contains("ROLE_ADMIN")) {
+					this.setDefaultTargetUrl("/admin");
+				} else if (roles.contains("ROLE_HR")) {
+					this.setDefaultTargetUrl("/hr");
+				} else if (roles.contains("ROLE_INTERVIEWER")){
+					this.setDefaultTargetUrl("/interviewer");
+				}
+				super.onAuthenticationSuccess(request, response, authentication);
+			}
+		};
 	}
 
 }
